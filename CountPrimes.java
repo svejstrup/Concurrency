0c436a7d42b42f33d404d15c06b75e573740dac3
svejstrup <madssvejstrup2@gmail.com>
2015-11-05 17:45:59 +0100

    First commit

---
+++ CountPrimes.java
@@ -0,0 +1,16 @@
+public class CountPrimes {
+
+    private static boolean isPrime(int n){
+	int k = 2;
+	while (k*k <= n && n % k != 0)
+	    k ++;
+	return n >=2 && k*k > n;
+    }
+
+    public static void main(String[] args){
+	long count = 0;
+	for(int i = 1; i<10000000; i++)
+	    if(isPrime(i)) count++;
+	System.out.println("Number of primes: " + count);
+    }
+}
